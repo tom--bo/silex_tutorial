03bac8ecbe3dbb5af1daed2a304ba0ce8f7b2ea7
tom--bo <otsuka.t.2013@gmail.com>
2014-12-19 01:30:57 +0900

    formをtwigで出すまで（フォームの送り先は未実装）
    web/index.php サイトが間違ってる部分等パスの修正あり
    view/ twigのコードで見た目部分作成

--- web/index.php
+++ web/index.php
@@ -2,9 +2,14 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
 
-$app->get('/hello/{name}', function ($name) use ($app) {
-    return 'Hello '.$app->escape($name);
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views',
+));
+
+$app->get('/member/register', function() use ($app) {
+    return $app['twig']->render('register.html.twig');
 });
 
 $app->run();
