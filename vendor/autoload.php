7991a5523c58788a2d7844f0bcd2c8cabfbaf8c0
tom--bo <otsuka.t.2013@gmail.com>
2014-12-19 01:19:08 +0900

    twigのインストール

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit8f86f1defb170f2bd8d4f372e4927d37::getLoader();
+return ComposerAutoloaderInit90a7adfa170dbd9467283e386217fdee::getLoader();
